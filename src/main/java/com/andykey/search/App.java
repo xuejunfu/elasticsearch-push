2cd9e4315a34a809733a82592baeab33464b28b9
fuxuejun <wangyong@git.dajiuxing.com.cn>
2017-05-08 14:35:00 +0800

    Initial commit

---
+++ src/main/java/com/andykey/search/App.java
@@ -0,0 +1,14 @@
+package com.andykey.search;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	ClassLoader classLoader = App.class.getClassLoader();
+    	System.out.println( "厉害了，我的哥！" );
+    }
+}
---
+++ src/main/java/com/andykey/search/Test.java
@@ -0,0 +1,11 @@
+package com.andykey.search;
+
+/**
+ * Created by Administrator on 2017/5/8.
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("sdfsadf");
+    }
+}
